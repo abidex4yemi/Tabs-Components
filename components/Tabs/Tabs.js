f67588c997f3537edd15317820a3830395da8cc9
abidex4yemi <abidex4yemi@gmail.com>
2019-05-16 13:30:33 +0100

    update clickable tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,16 +12,21 @@ class TabLink {
 		this.tabItem = new TabItem(this.itemElement);
 		// Add a click event listener on this instance, calling the select method on click
 
-		this.element.addEventListener('click', () => this.tabItem.select());
+		this.element.addEventListener('click', () => {
+			this.tabItem.select();
+			this.select();
+		});
 	}
 
 	select() {
 		// Get all of the elements with the tabs-link class
-		// const links;
+		const links = document.querySelectorAll('.tabs-link');
 		// Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-		// Array.from(links).forEach();
+		links.forEach(link => {
+			link.classList.remove('tabs-link-selected');
+		});
 		// Add a class named "tabs-link-selected" to this link
-		// this.element;
+		this.element.classList.add('tabs-link-selected');
 		// Call the select method on the item associated with this link
 	}
 }
@@ -35,7 +40,6 @@ class TabItem {
 	select() {
 		// Select all ".tabs-item" elements from the DOM
 		const items = document.querySelectorAll('.tabs-item');
-		console.log(items);
 		items.forEach(item => {
 			// Remove the class "tabs-item-selected" from each element
 			item.classList.remove('tabs-item-selected');
